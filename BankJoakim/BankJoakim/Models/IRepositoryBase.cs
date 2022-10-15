4cf99e173cc4de0c486dbae2bec893f1993516f7
Hassecore <joakim.ahman@gmail.com>
2022-10-15 13:39:56 +0200

    Created RepositoryBase.

---
+++ BankJoakim/BankJoakim/Models/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BankJoakim.Models
+{
+    public interface IRepositoryBase<T> where T: class, IEntityBase
+    {
+        int Count();
+        T GetById(Guid id);
+        IQueryable<T> Get(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Commit();
+    }
+}
