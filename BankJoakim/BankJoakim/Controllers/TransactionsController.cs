186f61e78b117528acf3cbc97095c9abe531872d
Hassecore <joakim.ahman@gmail.com>
2022-10-16 11:53:26 +0200

    Created controller and endpoint.

---
+++ BankJoakim/BankJoakim/Controllers/TransactionsController.cs
@@ -0,0 +1,34 @@
+﻿using BankJoakim.MediatR.Commands;
+using BankJoakim.Resources.Transactions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BankJoakim.Controllers
+{
+    public class TransactionsController : ControllerBase
+    {
+        readonly IMediator _mediator;
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> CreateTransaction([FromBody] TransactionCreateResource resource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var commandResult = await _mediator.Send(new TransactionCreateCommand(resource));
+
+            if (!commandResult.HasSucceeded)
+            {
+                return BadRequest(commandResult.ErrorMessage);
+            }
+
+            return Ok(commandResult.Resource);
+        }
+    }
+}
