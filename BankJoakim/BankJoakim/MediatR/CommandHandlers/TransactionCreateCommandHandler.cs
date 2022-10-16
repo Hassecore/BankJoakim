0434cbaaa2925e226cc2b553a13252891ddbbc2f
Hassecore <joakim.ahman@gmail.com>
2022-10-16 11:48:40 +0200

    Finished commandHandler.

--- BankJoakim/BankJoakim/MediatR/CommandHandlers/TransactionCreateCommandHandler.cs
+++ BankJoakim/BankJoakim/MediatR/CommandHandlers/TransactionCreateCommandHandler.cs
@@ -4,7 +4,6 @@ using BankJoakim.Models.Transactions;
 using BankJoakim.Resources.Transactions;
 using MediatR;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,16 +33,47 @@ namespace BankJoakim.MediatR.CommandHandlers
 
             if (sendingAccount == null)
             {
-
+                return Task.FromResult(new CommandResult<TransactionResource> { HasSucceeded = false, ErrorMessage = "Sending account not found." });
             }
 
             if (receivingAccount == null)
             {
-
+                return Task.FromResult(new CommandResult<TransactionResource> { HasSucceeded = false, ErrorMessage = "Receiving account not found." });
             }
 
+            if (sendingAccount.Balance < resource.Ammount)
+            {
+                return Task.FromResult(new CommandResult<TransactionResource> { HasSucceeded = false, ErrorMessage = "Insufficient balance." });
+            }
 
-            throw new NotImplementedException();
+            sendingAccount.Balance -= resource.Ammount;
+            receivingAccount.Balance += resource.Ammount;
+
+            var transaction = new Transaction
+            {
+                Id = Guid.NewGuid(),
+                Ammount = resource.Ammount,
+                SendingAccountId = resource.SendingAccountId,
+                SendingAccount = sendingAccount,
+                ReceivingAccountId = resource.ReceivingAccountId,
+                ReceivingAccount = receivingAccount
+            };
+
+            _transactionsRepository.Add(transaction);
+            _transactionsRepository.Commit();
+
+            return Task.FromResult(new CommandResult<TransactionResource>
+            {
+                HasSucceeded = true,
+                Resource = new TransactionResource
+                {
+                    Id = transaction.Id,
+                    Ammount = transaction.Ammount,
+                    CreatedOn = transaction.CreatedOn,
+                    SendingAccountId = transaction.SendingAccountId,
+                    ReceivingAccountId = transaction.ReceivingAccountId
+                }
+            });
         }
     }
 }
